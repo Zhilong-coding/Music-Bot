a66dd74dc63241bc5dae2c6cdfabb88d9987e846
Zhilong-coding <zhilong.k.liang@gmail.com>
2021-10-27 22:25:42 +0200

    Update main..py

--- main..py
+++ main..py
@@ -1,20 +1,18 @@
-from keep_alive import keep_alive
 import discord
 from discord.ext import commands
-from discord import FFmpegPCMAudio
+from discord.player import FFmpegPCMAudio
 from discord.utils import get
 import os
 from youtube_dl import YoutubeDL
 import asyncio
 from datetime import datetime, timedelta
-from dotenv import load_dotenv
+from dotenv import load_dotenv()
 
 load_dotenv()
 
-
 song_queue = []
 song = []
-client = commands.Bot(command_prefix="!", case_insensitive =True)
+client = commands.Bot(command_prefix="!", case_insensitive=True)
 
 ydl_opts = {
     'format': 'best',
@@ -22,10 +20,7 @@ ydl_opts = {
     'ignoreerrors': True,
     'logtostderr': False,
     'no_warnings': True,
-    'default_search': 'ytsearch1',
-    'buffer-size SIZE': 8000,
-    'username' : 'zhilong.k.liang@gmail.com',
-    'password' : os.getenv('password'),
+    'default_search': 'ytsearch1'
 }
 FFMPEG_OPTIONS = {
     'before_options':
@@ -33,12 +28,14 @@ FFMPEG_OPTIONS = {
     'options': '-vn'
 }
 
+
 @client.event
 async def on_ready():
-  print("Bot is now online...")
-  await client.change_presence(activity=discord.Game(name="TAKO | !play"))
+    print("Bot is now online...")
+    await client.change_presence(activity=discord.Game(name="TAKO | !play"))
 
-@client.command(aliases=['p', 'pl','pla','sing','music','song','adin'])
+
+@client.command(aliases=['p', 'pl', 'pla', 'sing', 'music', 'song', 'adin'])
 async def play(ctx, *args):
     url = ' '.join(args)
     global song_queue
@@ -53,72 +50,90 @@ async def play(ctx, *args):
         await ctx.send('TAKO Does No Understand')
         return
     song_queue.append(url)
-    await ctx.send(f"Searching...")
+    await ctx.send(f"Searching... {url.upper()}")
     #song_queue.append(song)
     global song
     try:
-      if not voice.is_playing() and not voice.is_paused():
-          with YoutubeDL(ydl_opts) as ydl:
-              info = ydl.extract_info(song_queue[0], download=False)
-          if info.get('url', None):
-              URL = info['url']
-              TITLE = info['title']
-              DURATION = info['duration']
-              THUMBNAIL = info['thumbnail']
-          else:
-              URL = info['entries'][0]['url']
-              TITLE = info['entries'][0]['title']
-              DURATION = info['entries'][0]['duration']
-              THUMBNAIL = info['entries'][0]['thumbnail']
-          song = {"title": TITLE, "url": URL, "duration": DURATION, "thumbnail" : THUMBNAIL}
-          del song_queue[0]
-          voice.play(discord.PCMVolumeTransformer(FFmpegPCMAudio(song['url'], **FFMPEG_OPTIONS), volume=0.3),
-                    after=lambda e: play_next(ctx))
-          voice.is_playing()
-          x = divmod(song['duration'], 60)
-          y = str(f"{x[0]:02d}") + ":" + str(f"{x[1]:02d}")
-          now = datetime.now() + timedelta(hours=2)
-          current_time = now.strftime("%H:%M")
-          embed = discord.Embed(title=song['title'], description='Requested by: \n'+ str(ctx.message.author), color=discord.Color.purple())
-          embed.set_thumbnail(url=song['thumbnail'])
-          embed.add_field(name='Status',value='Now Playing',inline=True)
-          embed.add_field(name='Duration', value=y)
-          embed.set_footer(text=current_time)
-          #await ctx.send(f"Now playing: {song['title']} | Song Duration: {y}")
-          await ctx.send(embed=embed)
-          song.clear()
-      else:
-        with YoutubeDL(ydl_opts) as ydl:
-          info = ydl.extract_info(song_queue[len(song_queue)-1], download=False)
-          if info.get('url', None):
-              URL = info['url']
-              TITLE = info['title']
-              DURATION = info['duration']
-              THUMBNAIL = info['thumbnail']
-          else:
-              URL = info['entries'][0]['url']
-              TITLE = info['entries'][0]['title']
-              DURATION = info['entries'][0]['duration']
-              THUMBNAIL = info['entries'][0]['thumbnail']
-          song = {"title": TITLE, "url": URL, "duration": DURATION, "thumbnail" : THUMBNAIL, "author": str(ctx.message.author)}
-          x = divmod(song['duration'], 60)
-          y = str(f"{x[0]:02d}") + ":" + str(f"{x[1]:02d}")
-          embed = discord.Embed(title="Added to Queue", description="Requested By: \n" + song['author'], color=discord.Color.purple())
-          embed.set_thumbnail(url=song['thumbnail'])
-          embed.add_field(name='Song',value=song['title'] ,inline=True)
-          embed.add_field(name='Duration', value=y)
-          #await ctx.send(f"Added to Queue: {song['title']} | Song Duration: {y}")
-          await ctx.send(embed=embed)
-          song.clear()
+        if not voice.is_playing() and not voice.is_paused():
+            with YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info("'"+
+                  song_queue[0]+"'", download=False)
+            if info.get('url', None):
+                data = info
+            else:
+                data = info["entries"][0]
+            TITLE = data['title']
+            URL = data['url']
+            DURATION = data['duration']
+            THUMBNAIL = data['thumbnail']
+            song = {
+                "title": TITLE,
+                "url": URL,
+                "duration": DURATION,
+                "thumbnail": THUMBNAIL
+            }
+            del song_queue[0]
+            voice.play(discord.PCMVolumeTransformer(FFmpegPCMAudio(
+                song['url'], **FFMPEG_OPTIONS),
+                                                    volume=0.3),
+                       after=lambda e: play_next(ctx))
+            voice.is_playing()
+            x = divmod(song['duration'], 60)
+            y = str(f"{x[0]:02d}") + ":" + str(f"{x[1]:02d}")
+            now = datetime.now() + timedelta(hours=2)
+            current_time = now.strftime("%H:%M")
+            embed = discord.Embed(title=song['title'],
+                                  description='Requested by: \n' +
+                                  str(ctx.message.author),
+                                  color=discord.Color.purple())
+            embed.set_thumbnail(url=song['thumbnail'])
+            embed.add_field(name='Status', value='Now Playing', inline=True)
+            embed.add_field(name='Duration', value=y)
+            embed.set_footer(text=current_time)
+            #await ctx.send(f"Now playing: {song['title']} | Song Duration: {y}")
+            await ctx.send(embed=embed)
+            song.clear()
+        else:
+            with YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info("'"+song_queue[len(song_queue)-1]+"'", download=False)
+                if info.get("url", None):
+                    data = info
+                else:
+                    data = info["entries"][0]
+                TITLE = data['title']
+                URL = data['url']
+                DURATION = data['duration']
+                THUMBNAIL = data['thumbnail']
+                song = {
+                    "title": TITLE,
+                    "url": URL,
+                    "duration": DURATION,
+                    "thumbnail": THUMBNAIL,
+                    'author': str(ctx.message.author)
+                }
+                x = divmod(song['duration'], 60)
+                y = str(f"{x[0]:02d}") + ":" + str(f"{x[1]:02d}")
+                embed = discord.Embed(title="Added to Queue",
+                                      description="Requested By: \n" +
+                                      song['author'],
+                                      color=discord.Color.purple())
+                embed.set_thumbnail(url=song['thumbnail'])
+                embed.add_field(name='Song', value=song['title'], inline=True)
+                embed.add_field(name='Duration', value=y)
+                #await ctx.send(f"Added to Queue: {song['title']} | Song Duration: {y}")
+                await ctx.send(embed=embed)
+                song.clear()
     except Exception as e:
-      await ctx.send(f"Something Went doodoo")
-      await ctx.send(e)
-      return
+        await ctx.send(f"Something Went doodoo")
+        await ctx.send(e)
+        return
 
 
 @client.command()
 async def leave(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+      voice.stop()
     if voice.is_connected():
         await voice.disconnect()
 
@@ -149,31 +164,42 @@ async def stop(ctx):
 
 def play_next(ctx):
     voice = get(client.voice_clients, guild=ctx.guild)
-    if song_queue:
+    if len(
+      song_queue) > 0:
         with YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(song_queue[0], download=False)
-        if info.get('url', None):
-            URL = info['url']
-            TITLE = info['title']
-            DURATION = info['duration']
-            THUMBNAIL = info['thumbnail']
-        else:
-            URL = info['entries'][0]['url']
-            TITLE = info['entries'][0]['title']
-            DURATION = info['entries'][0]['duration']
-            THUMBNAIL = info['entries'][0]['thumbnail']
-        song = {"title": TITLE, "url": URL, "duration": DURATION, "thumbnail" : THUMBNAIL, "author": str(ctx.message.author)}
+            info = ydl.extract_info("'"+song_queue[0]+"'", download=False)
+            if info.get("url", None):
+                data = info
+            else:
+                data = info["entries"][0]
+            TITLE = data['title']
+            URL = data['url']
+            DURATION = data['duration']
+            THUMBNAIL = data['thumbnail']
+            song = {
+                "title": TITLE,
+                "url": URL,
+                "duration": DURATION,
+                "thumbnail": THUMBNAIL,
+                'author': str(ctx.message.author)
+            }
         del song_queue[0]
-        voice.play(discord.PCMVolumeTransformer(FFmpegPCMAudio(song['url'], **FFMPEG_OPTIONS), volume=0.3), after=lambda e: play_next(ctx))
+        voice.play(discord.PCMVolumeTransformer(FFmpegPCMAudio(
+            song['url'], **FFMPEG_OPTIONS),
+                                                volume=0.3),
+                   after=lambda e: play_next(ctx))
         voice.is_playing()
         x = divmod(song['duration'], 60)
         y = str(f"{x[0]:02d}") + ":" + str(f"{x[1]:02d}")
         #ctx.send(f"Now playing: {song['title']} | Song Duration: {y}")
         now = datetime.now() + timedelta(hours=2)
         current_time = now.strftime("%H:%M")
-        embed = discord.Embed(title=song['title'], description='Requested by: \n'+ str(ctx.message.author), color=discord.Color.purple())
+        embed = discord.Embed(title=song['title'],
+                              description='Requested by: \n' +
+                              str(ctx.message.author),
+                              color=discord.Color.purple())
         embed.set_thumbnail(url=song['thumbnail'])
-        embed.add_field(name='Status',value='Now Playing',inline=True)
+        embed.add_field(name='Status', value='Now Playing', inline=True)
         embed.add_field(name='Duration', value=y)
         embed.set_footer(text=current_time)
         coro = ctx.send(embed=embed)
@@ -187,38 +213,48 @@ def play_next(ctx):
             pass
 
 
-@client.command(pass_context=True, aliases=['s','fs'])
+@client.command(pass_context=True, aliases=['s', 'fs'])
 async def skip(ctx):
     voice = get(client.voice_clients, guild=ctx.guild)
     voice.stop()
-    if song_queue:
-      with YoutubeDL(ydl_opts) as ydl:
-          info = ydl.extract_info(song_queue[0], download=False)
-      if info.get('url', None):
-          URL = info['url']
-          TITLE = info['title']
-          DURATION = info['duration']
-          THUMBNAIL = info['thumbnail']
-      else:
-          URL = info['entries'][0]['url']
-          TITLE = info['entries'][0]['title']
-          DURATION = info['entries'][0]['duration']
-          THUMBNAIL = info['entries'][0]['thumbnail']
-      song = {"title": TITLE, "url": URL, "duration": DURATION, "thumbnail" : THUMBNAIL, "author": str(ctx.message.author)}
-    
-      voice.play(discord.PCMVolumeTransformer(FFmpegPCMAudio(song['url'], **FFMPEG_OPTIONS), volume=0.3),
-            after=lambda e: play_next(ctx))
-      voice.is_playing()
-      x = divmod(song['duration'], 60)
-      y = str(f"{x[0]:02d}") + ":" + str(f"{x[1]:02d}")
-      now = datetime.now() + timedelta(hours=2)
-      current_time = now.strftime("%H:%M")
-      embed = discord.Embed(title=song['title'], description='Requested by: \n'+ str(ctx.message.author), color=discord.Color.purple())
-      embed.set_thumbnail(url=song['thumbnail'])
-      embed.add_field(name='Status',value='Now Playing',inline=True)
-      embed.add_field(name='Duration', value=y)
-      embed.set_footer(text=current_time)
-      await ctx.send(embed=embed)
+    if len(song_queue) > 0:
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info("'"+song_queue[0]+"'", download=False)
+            if info.get("url", None):
+                data = info
+            else:
+                data = info["entries"][0]
+            TITLE = data['title']
+            URL = data['url']
+            DURATION = data['duration']
+            THUMBNAIL = data['thumbnail']
+            song = {
+                "title": TITLE,
+                "url": URL,
+                "duration": DURATION,
+                "thumbnail": THUMBNAIL,
+                'author': str(ctx.message.author)
+            }
+        del song_queue[0]
+        voice.play(discord.PCMVolumeTransformer(FFmpegPCMAudio(
+            song['url'], **FFMPEG_OPTIONS),
+                                                volume=0.3),
+                   after=lambda e: play_next(ctx))
+        voice.is_playing()
+        x = divmod(song['duration'], 60)
+        y = str(f"{x[0]:02d}") + ":" + str(f"{x[1]:02d}")
+        now = datetime.now() + timedelta(hours=2)
+        current_time = now.strftime("%H:%M")
+        embed = discord.Embed(title=song['title'],
+                              description='Requested by: \n' +
+                              str(ctx.message.author),
+                              color=discord.Color.purple())
+        embed.set_thumbnail(url=song['thumbnail'])
+        embed.add_field(name='Status', value='Now Playing', inline=True)
+        embed.add_field(name='Duration', value=y)
+        embed.set_footer(text=current_time)
+        await ctx.send(embed=embed)
+
 
 @client.command(aliases=['ps'])
 async def playskip(ctx, *args):
@@ -229,32 +265,42 @@ async def playskip(ctx, *args):
         song_queue.clear()
         song_queue.append(url)
         with YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(song_queue[0], download=False)
-        if info.get('url', None):
-            URL = info['url']
-            TITLE = info['title']
-            DURATION = info['duration']
-            THUMBNAIL = info['thumbnail']
-        else:
-            URL = info['entries'][0]['url']
-            TITLE = info['entries'][0]['title']
-            DURATION = info['entries'][0]['duration']
-            THUMBNAIL = info['entries'][0]['thumbnail']
-        song = {"title": TITLE, "url": URL, "duration": DURATION, "thumbnail" : THUMBNAIL, "author": str(ctx.message.author)}
+            info = ydl.extract_info("'"+song_queue[0]+"'", download=False)
+            if info.get("url", None):
+                data = info
+            else:
+                data = info["entries"][0]
+            TITLE = data['title']
+            URL = data['url']
+            DURATION = data['duration']
+            THUMBNAIL = data['thumbnail']
+            song = {
+                "title": TITLE,
+                "url": URL,
+                "duration": DURATION,
+                "thumbnail": THUMBNAIL,
+                'author': str(ctx.message.author)
+            }
         del song_queue[0]
-        voice.play(discord.PCMVolumeTransformer(FFmpegPCMAudio(song['url'], **FFMPEG_OPTIONS), volume=0.3),
+        voice.play(discord.PCMVolumeTransformer(FFmpegPCMAudio(
+            song['url'], **FFMPEG_OPTIONS),
+                                                volume=0.3),
                    after=lambda e: play_next(ctx))
         voice.is_playing()
         x = divmod(song['duration'], 60)
         y = str(f"{x[0]:02d}") + ":" + str(f"{x[1]:02d}")
         now = datetime.now() + timedelta(hours=2)
         current_time = now.strftime("%H:%M")
-        embed = discord.Embed(title=song['title'], description='Requested by: \n'+ str(ctx.message.author), color=discord.Color.purple())
+        embed = discord.Embed(title=song['title'],
+                              description='Requested by: \n' +
+                              str(ctx.message.author),
+                              color=discord.Color.purple())
         embed.set_thumbnail(url=song['thumbnail'])
-        embed.add_field(name='Status',value='Now Playing',inline=True)
+        embed.add_field(name='Status', value='Now Playing', inline=True)
         embed.add_field(name='Duration', value=y)
         embed.set_footer(text=current_time)
         await ctx.send(embed=embed)
+
 
 @client.command(aliases=['pn'])
 async def playnext(ctx, *args):
@@ -262,33 +308,41 @@ async def playnext(ctx, *args):
     song_queue.append(url)
     song_queue.insert(0, song_queue.pop())
     with YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(song_queue[0], download=False)
-        if info.get('url', None):
-          URL = info['url']
-          TITLE = info['title']
-          DURATION = info['duration']
-          THUMBNAIL = info['thumbnail']
+        info = ydl.extract_info("'"+song_queue[0]+"'", download=False)
+        if info.get("url", None):
+            data = info
         else:
-          URL = info['entries'][0]['url']
-          TITLE = info['entries'][0]['title']
-          DURATION = info['entries'][0]['duration']
-          THUMBNAIL = info['entries'][0]['thumbnail']
-        song = {"title": TITLE, "url": URL, "duration": DURATION, "thumbnail" : THUMBNAIL, "author": str(ctx.message.author)}
+            data = info["entries"][0]
+        TITLE = data['title']
+        URL = data['url']
+        DURATION = data['duration']
+        THUMBNAIL = data['thumbnail']
+        song = {
+            "title": TITLE,
+            "url": URL,
+            "duration": DURATION,
+            "thumbnail": THUMBNAIL,
+            'author': str(ctx.message.author)
+        }
         x = divmod(song['duration'], 60)
         y = str(f"{x[0]:02d}") + ":" + str(f"{x[1]:02d}")
         now = datetime.now() + timedelta(hours=2)
         current_time = now.strftime("%H:%M")
-        embed = discord.Embed(title='Playing Next', description='Requested by: \n'+ str(ctx.message.author), color=discord.Color.purple())
+        embed = discord.Embed(title='Playing Next',
+                              description='Requested by: \n' +
+                              str(ctx.message.author),
+                              color=discord.Color.purple())
         embed.set_thumbnail(url=song['thumbnail'])
-        embed.add_field(name='Song',value=song['title'],inline=True)
+        embed.add_field(name='Song', value=song['title'], inline=True)
         embed.add_field(name='Duration', value=y)
         embed.set_footer(text=current_time)
         await ctx.send(embed=embed)
 
+
 @client.command(aliases=['q'])
 async def queuelist(ctx):
-  for x in range(len(song_queue)):
-    await ctx.send(f"{x+1}. {song_queue[x]}")
+    for x in range(len(song_queue)):
+        await ctx.send(f"{x+1}. {song_queue[x]}")
 
-keep_alive()
+
 client.run(os.getenv('TOKEN'))
